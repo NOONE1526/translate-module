4e9083d84e097cad57f810a52bdaea5e7784b147
Nick <52355162+NOONE1526@users.noreply.github.com>
2022-09-26 13:43:58 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='translate-module',
+    version='0.1',
+    author="NOONE1526",
+    author_email='140539lil@gmail.com',
+    packages=find_packages(''),
+    package_dir={'': ''},
+    url='https://github.com/NOONE1526/translate-module',
+    keywords='example project'
+)
