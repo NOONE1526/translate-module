68ea2bb5588aae1d5071d530068953a798dd505e
Nick <52355162+NOONE1526@users.noreply.github.com>
2022-09-26 13:55:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     version='0.1',
     author="NOONE1526",
     author_email='140539lil@gmail.com',
-    packages=find_packages(''),
-    package_dir={'': ''},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     url='https://github.com/NOONE1526/translate-module',
     keywords='example project'
 )
